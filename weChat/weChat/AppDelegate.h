07e66b08da0a956bdb1527c9f5f4858ab58033e4
ZBC <565618435@qq.com>
2016-03-03 19:28:12 +0800

    初始化项目

---
+++ weChat/weChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weChat
+//
+//  Created by zack on 16/3/3.
+//  Copyright © 2016年 zack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
